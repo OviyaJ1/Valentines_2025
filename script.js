928543e94e230186f54e41568187ff1c95cc25ec
Oviya Jeyaprakash <oviyaj1@gmail.com>
2025-02-04 21:12:37 -0600

    Initial Setup

--- script.js
+++ script.js
@@ -4,11 +4,8 @@
 function selectOption(option) {
     // Check which option was clicked
     if (option === 'yes') {
-        // Flash rainbow colors
-        flashRainbowColors(function() {
-            document.getElementById('question').style.display = 'none'; // Hide the question
-            displayCatHeart(); // Display the cat-heart.gif
-        });
+        document.getElementById('question').style.display = 'none'; // Hide the question
+        displayCatHeart(); // Display the cat-heart.gif
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
         document.getElementById('no-button').innerText = 'You sure?'; 
