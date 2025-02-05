219740ebe314518944832551909639ba9c7b8e26
Oviya Jeyaprakash <oviyaj1@gmail.com>
2025-02-04 21:24:39 -0600

    Edit fonts and gifs

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ function selectOption(option) {
 }
 
 // Function to display the cat.gif initially
-function displayCat() {
+function displayImage() {
     // Get the container where the image will be displayed
     var imageContainer = document.getElementById('image-container');
     // Create a new Image element for the cat
     var catImage = new Image();
     // Set the source (file path) for the cat image
-    catImage.src = './images/intial_question.gif'; // Assuming the cat image is named "cat.gif"
+    catImage.src = './images/initial_question.gif'; // Assuming the cat image is named "cat.gif"
     // When the cat image is fully loaded, add it to the image container
     catImage.onload = function() {
         imageContainer.appendChild(catImage);
@@ -52,22 +52,22 @@ function displayCatHeart() {
     };
 }
 
-// Display the cat.gif initially
-displayCat();
+// Display the .gif initially
+displayImage();
 
 // Function to flash rainbow colors and then execute a callback function
-function flashRainbowColors(callback) { //HE WILL DIE!!!!!!!
-    var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
-    var i = 0;
-    var interval = setInterval(function() {
-        document.body.style.backgroundColor = colors[i];
-        i = (i + 1) % colors.length;
-    }, 200); // Change color every 200 milliseconds
-    setTimeout(function() {
-        clearInterval(interval);
-        document.body.style.backgroundColor = ''; // Reset background color
-        if (callback) {
-            callback();
-        }
-    }, 2000); // Flash colors for 2 seconds
-}
+// function flashRainbowColors(callback) { //HE WILL DIE!!!!!!!
+//     var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
+//     var i = 0;
+//     var interval = setInterval(function() {
+//         document.body.style.backgroundColor = colors[i];
+//         i = (i + 1) % colors.length;
+//     }, 200); // Change color every 200 milliseconds
+//     setTimeout(function() {
+//         clearInterval(interval);
+//         document.body.style.backgroundColor = ''; // Reset background color
+//         if (callback) {
+//             callback();
+//         }
+//     }, 2000); // Flash colors for 2 seconds
+// }
